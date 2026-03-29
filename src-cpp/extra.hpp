b34ba5168719daf8d34643826a4aa68643b0a589
keychera <kyogatama@gmail.com>
2026-03-29 20:12:08 +0900

    texquad vertex buffer and index buffer

--- src-cpp/extra.hpp
+++ src-cpp/extra.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <SDL3/SDL.h>
 #include <cstddef>
 
 namespace extra {
@@ -17,4 +18,16 @@ static UniformBuffer timeUniform{};
 
 // https://clojurians.slack.com/archives/C03SRH97FDK/p1757647872126659?thread_ts=1757644640.338809&cid=C03SRH97FDK
 inline void temp_aset(Vertex *p, size_t i, Vertex t) { p[i] = t; }
+
+typedef struct PositionTextureVertex {
+  float x, y, z;
+  float u, v;
+} PositionTextureVertex;
+
+inline void aset(PositionTextureVertex *p, size_t i, PositionTextureVertex t) {
+  p[i] = t;
+}
+
+inline void aset(Uint16 *p, size_t i, Uint16 t) { p[i] = t; }
+
 } // namespace extra
